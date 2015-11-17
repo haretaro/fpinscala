58b89e4de91361a9240a0aceb191991a5336ebd1
haretaro <frontend.1000@gmail.com>
2015-11-17 22:41:04 +0900

    コメントを追加

--- src/main/scala/gettingstarted/GettingStarted.scala
+++ src/main/scala/gettingstarted/GettingStarted.scala
@@ -8,6 +8,7 @@ object MyModule{
 
   def curry[A,B,C](f: (A,B) => C):A => (B => C) = a => b => f(a,b)
 
+  //末尾再帰で書いたフィボナッチ
   def fibonacci(x:Int) = {
     def go(n:Int, a:Int, b:Int) :Int = 
       if(n<2) a
@@ -21,10 +22,11 @@ object MyModule{
   }
 
   def isSorted[A](as:Array[A], orderd:(A,A) => Boolean) :Boolean = {
+    //size, tail はArrayのメソッドでないので暗黙の型変換(implicit convert)が発生する
     def go(array:Array[A]):Boolean = {
-      if(array.size < 2) true
+      if(array.size < 2) true//lengthを呼べば暗黙型変換は発生しない
       else if(orderd(array(0),array(1))==false) false
-      else go(array.tail)
+      else go(array.tail)//tailが呼ばれる度に配列のコピーが生成される
     }
     go(as)
   }
